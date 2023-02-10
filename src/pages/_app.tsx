2641abf5560e4ab3f2f86c62dfcabf2ab50b704d
Maicon P. Del Bel <maicon.delbel@gmail.com>
2023-02-10 10:36:17 -0300

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/global'
+import { defaultTheme } from './../styles/theme/default'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  // Global Styles CSS
+  html,
+  body {
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`
